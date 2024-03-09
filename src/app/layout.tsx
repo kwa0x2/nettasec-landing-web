2223a35d148bf92d2ba73115f98153a9235ba39a
kwasec <kwasec@nettasec.com>
2024-03-09 21:54:19 +0300

    Initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,33 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import { Toaster } from "@/components/ui/toaster"
+
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  icons: "https://cdn.discordapp.com/attachments/1166821043743236218/1194009838196359228/nettasec-icon.png?ex=65ef64d1&is=65dcefd1&hm=2d5bb4b2ba1bdfa55657b19906dbc317c835680b21a89cbbb7fd107d1bcfe8e4&",
+  title: "Comming soon.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className} suppressHydrationWarning={true}>
+        {children}{" "}
+        <footer className="fixed bottom-0 w-full bg-transparent text-neutral-500 p-4 text-center">
+          <p>
+            ©2024 <span className="text-[#a44246] font-bold">NettaSec Solutions</span>
+          </p>
+        </footer>
+        <Toaster />
+
+      </body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,81 @@
+"use client";
+import { useState } from "react";
+import { BackgroundBeams } from "@/components/ui/background-beams";
+import { useToast } from "@/components/ui/use-toast";
+
+export default function Home() {
+  const { toast } = useToast();
+  const [email, setEmail] = useState("");
+
+  const handleSubmit = async () => {
+    try {
+      const response = await fetch("http://localhost:9898/api/waitlist", {
+        method: "POST",
+        body: JSON.stringify({ email }),
+      });
+
+      if (response.status === 201) {
+        const responseData = await response.json();
+
+        if (responseData.rowAffected > 0) {
+          toast({
+            title: "Thank You!",
+            description:
+              "Your email address has been successfully added. You will receive updates soon!",
+          });
+        }
+      } else if (response.status === 409) {
+        const responseData = await response.json();
+
+        if (responseData.error) {
+          toast({
+            variant: "destructive",
+            title: "Error Occurred!",
+            description: "The email address already exists.",
+          });
+        }
+      }
+    } catch (error) {
+      toast({
+        variant: "destructive",
+        title: "Uh oh! Something went wrong.",
+        description: "There was a problem with your request.",
+      });
+    }
+  };
+
+  return (
+    <div className="h-screen bg-neutral-950 relative flex flex-col items-center justify-center antialiased">
+      <div className="max-w-2xl mx-auto p-5">
+        <h1 className="relative z-10 text-lg md:text-7xl bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-600 text-center font-sans font-bold">
+          Join the waitlist
+        </h1>
+        <p></p>
+        <p className="text-neutral-500 max-w-lg mx-auto my-5 text-sm text-center relative z-10">
+          We are shaping the future of cybersecurity, software development, and
+          cloud solutions. Follow us as we pave the way for a more secure and
+          innovative digital landscape. Stay tuned for more updates!
+        </p>
+        <div className="flex justify-center items-center">
+          <input
+            type="text"
+            placeholder="hi@nettasec.com"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className="rounded-lg border border-neutral-800 focus:ring-2 focus:ring-teal-500 w-80 sm:w-1/2 sm:max-w-md relative z-10 mt-2 p-3 bg-neutral-950 placeholder:text-neutral-700 text-neutral-400"
+            style={{ flex: "8" }}
+          />
+          <button
+            type="button"
+            onClick={handleSubmit}
+            className="rounded-lg border border-neutral-800 focus:ring-2 focus:ring-teal-500 ml-2 w-20 sm:w-1/2 sm:max-w-md relative z-10 mt-2 p-3 bg-neutral-950 placeholder:text-neutral-700 text-neutral-400"
+            style={{ flex: "2" }}
+          >
+            Submit
+          </button>
+        </div>
+      </div>
+      <BackgroundBeams />
+    </div>
+  );
+}
